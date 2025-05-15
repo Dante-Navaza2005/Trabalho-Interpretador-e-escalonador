7b5924c76882dab0ac8f10280961057cd947b2da
Dante <dantenavaza2005@gmail.com>
2025-05-15 09:32:29 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -1,5 +1,6 @@
 /*
 vai ter que fazer o tempo de espera do interpretador 1 segundo
 vai fazer teste com 6 programas
-
+pode botar um while inves do p1 em si
+fazer escaloador com 3 politicas, round robin, tempo real prioridade
 */ 
