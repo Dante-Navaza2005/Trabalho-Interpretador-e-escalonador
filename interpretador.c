94c044ae4c35d41a89dfc033310489ecb87de8de
marcela-05 <132473980+marcela-05@users.noreply.github.com>
2025-05-26 11:36:41 -0300

    Update interpretador.c

--- interpretador.c
+++ interpretador.c
@@ -1,14 +1,18 @@
-// interpretador.c
+// Marcela Issa 2310746 e Dante Navaza 2321406
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-#define UT 1                /* unidade de tempo entre comandos */
+#define UT 1  
+
+// Interpretador que lê comandos de exec.txt e os envia para o escalonador e 
+//envia um por um, a cada 1 UT, para o escalonador via stdout (pipe)
 
 int main(void) {
     fprintf(stderr, "[Interpretador] Iniciando leitura de comandos.\n");
 
+    // Abre o arquivo exec.txt para leitura
     FILE *arquivo = fopen("exec.txt", "r");
     if (!arquivo) {
         perror("[Interpretador] Erro ao abrir exec.txt");
@@ -17,9 +21,12 @@ int main(void) {
 
     char linha[256];
     while (fgets(linha, sizeof(linha), arquivo)) {
-        linha[strcspn(linha, "\n")] = '\0';          /* remove '\n' */
+        linha[strcspn(linha, "\n")] = '\0';  // Remove o \n no final da linha
         fprintf(stderr, "[Interpretador] Enviando: %s\n", linha);
-        write(STDOUT_FILENO, linha, strlen(linha) + 1); /* +1 => inclui '\0' */
+        // Envia o comando para o escalonador via pipe (stdout)
+        // Enviamos strlen(linha) + 1 para incluir o '\0' no final
+        write(STDOUT_FILENO, linha, strlen(linha) + 1); 
+        // Aguarda 1 unidade de tempo antes de enviar o próximo comando
         sleep(UT);
     }
 
